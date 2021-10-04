97142ada2198846eb92d36b03f345bf67e543e60
Lechuck Roh <lechuckroh@gmail.com>
2021-10-04 23:07:53 +0900

    octopus v2.0.0

--- Formula/octopus-db-tool.rb
+++ Formula/octopus-db-tool.rb
@@ -1,25 +1,29 @@
 class OctopusDbTool < Formula
     desc "DB schema tools with format conversion, source generation, reverse/forward engineering"
     homepage "https://github.com/lechuckroh/octopus-db-tool"
-    version "2.0.0-beta3"
+    version "2.0.0"
     license "MIT"
     bottle :unneeded
   
     on_macos do
       if Hardware::CPU.intel?
-        url "https://github.com/lechuckroh/octopus-db-tool/releases/download/2.0.0-beta3/oct-darwin64.gz", :using => CurlDownloadStrategy
-        sha256 "938d026d6fa9f0f78593d4edea0779c8563f85a1d9292b740176a140fc168965"
+        url "https://github.com/lechuckroh/octopus-db-tool/releases/download/v2.0.0/oct-darwin-amd64-v2.0.0.zip", :using => CurlDownloadStrategy
+        sha256 "6ffd8d364dff46c264536df64751147b201efeb827efe66efcbeb71bc9541952"
       end
       if Hardware::CPU.arm?
-        url "https://github.com/lechuckroh/octopus-db-tool/releases/download/2.0.0-beta3/oct-darwin64.gz", :using => CurlDownloadStrategy
-        sha256 "938d026d6fa9f0f78593d4edea0779c8563f85a1d9292b740176a140fc168965"
+        url "https://github.com/lechuckroh/octopus-db-tool/releases/download/v2.0.0/oct-darwin-arm64-v2.0.0.zip", :using => CurlDownloadStrategy
+        sha256 "ff79e99b060bb30576d640956c38145946410825bedf25e35fd63f7fb2b56589"
       end
     end
   
     on_linux do
       if Hardware::CPU.intel?
-        url "https://github.com/lechuckroh/octopus-db-tool/releases/download/2.0.0-beta3/oct-linux64.gz", :using => CurlDownloadStrategy
-        sha256 "a8f408f31f4e24888bfdd466ac51a1a9e7167608c16a69df308085176d711117"
+        url "https://github.com/lechuckroh/octopus-db-tool/releases/download/v2.0.0/oct-linux-amd64-v2.0.0.zip", :using => CurlDownloadStrategy
+        sha256 "a9afd05dab3490afd9e275fccdc82316575957687fadee6e554157c327df9c71"
+      end
+      if Hardware::CPU.arm?
+        url "https://github.com/lechuckroh/octopus-db-tool/releases/download/v2.0.0/oct-linux-arm64-v2.0.0.zip", :using => CurlDownloadStrategy
+        sha256 "d810a03b1f6a2709757d613f52509a6f85951b790e8aa115f20de2469c069010"
       end
     end
   
